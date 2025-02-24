16da18a185cff5e36f5fa01d5b44491a6e428dd5
Martin Seidl <bigibosik@gmail.com>
2025-02-24 13:46:47 +0100

    Napojena databaze
    Postgre napojeno a vytvorena tabulka

--- backend/src/main/java/cz/upce/fei/nnpiacv/domain/User.java
+++ backend/src/main/java/cz/upce/fei/nnpiacv/domain/User.java
@@ -1,11 +1,21 @@
 package cz.upce.fei.nnpiacv.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
+@Entity
+@Table(name="app_user")
 public class User {
+    @Id
     private long id;
     private String email;
     private String password;
+
+    public User() {
+
+    }
 }
